8e20db5c4ac187e44a4eb48f83373587f7b8933e
intrlocutr <45053302+intrlocutr@users.noreply.github.com>
2021-11-14 16:06:57 -0500

    Add database model and provider

---
+++ lib/db/provider.dart
@@ -0,0 +1,87 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:untether/model/reports.dart';
+
+class ReportDatabase {
+
+  // Constants used by the database.
+  static const String _dbName = 'report_database.db';
+  static const String _tblName = 'report';
+  static const int _dbVersion = 1;
+
+  // The ReportDatabase model will be a singleton.
+  ReportDatabase._privateConstructor();
+  static final ReportDatabase _instance = ReportDatabase._privateConstructor();
+  factory ReportDatabase() => _instance;
+
+  static Database? _db;
+
+  Future<Database?> get database async {
+    if (_db != null) return _db;
+    _db = await initDb();
+    return _db;
+  }
+
+  Future<Database> initDb() async {
+    return await openDatabase(
+        join(await getDatabasesPath(), _dbName),
+        version: _dbVersion,
+        onCreate: _onCreate
+    );
+  }
+
+  Future _onCreate(Database db, int version) {
+    return db.execute(
+      'CREATE TABLE $_tblName(timestamp INTEGER PRIMARY KEY, usageMinutes INTEGER, score DOUBLE)',
+    );
+  }
+
+  /// Inserts a `report` into the database.
+  Future insertReport(Report report) async {
+    Database? db = await _instance.database;
+    await db!.insert(
+      _tblName,
+      report.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  /// Reads the report from the day of `timestamp`.
+  Future<Report> readReport(DateTime timestamp) async {
+    Database? db = await _instance.database;
+
+    // Get the times in between which we should be looking.
+    // [gteTime, ltTime)
+    final DateTime minDate = DateTime(timestamp.year, timestamp.month, timestamp.day);
+    final int gteTime = minDate.millisecondsSinceEpoch;
+    final int ltTime = minDate.add(const Duration(days: 1)).millisecondsSinceEpoch;
+
+    final List<Map<String, dynamic>> q = await db!.query(
+        _tblName,
+        where: 'timestamp >= $gteTime AND timestamp < $ltTime'
+    );
+
+    return Report.fromMap(q[0]);
+  }
+
+  /// Updates the report in the database with `report`.
+  Future updateReport(Report report) async {
+    Database? db = await _instance.database;
+    await db!.update(
+        _tblName,
+        report.toMap(),
+        where: 'timestamp = ?',
+        whereArgs: [report.timestamp.millisecondsSinceEpoch]
+    );
+  }
+
+  /// Removes the report matching `report` from the database.
+  Future deleteReport(Report report) async {
+    Database? db = await _instance.database;
+    await db!.delete(
+        _tblName,
+        where: 'timestamp = ?',
+        whereArgs: [report.timestamp.millisecondsSinceEpoch]
+    );
+  }
+}
---
+++ lib/model/reports.dart
@@ -0,0 +1,46 @@
+/*import 'package:flutter/material.dart';
+
+/// A report for a specific date range.
+class RangeReport {
+  /// The time span of the reports.
+  late final DateTimeRange range;
+  late final List<Report> reports;
+}*/
+
+/// A report for a single day.
+class Report {
+  /// Timestamp of this report.
+  final DateTime timestamp;
+
+  /// Minutes of usage since the start of the day.
+  final int usageMinutes;
+
+  /// Score on the mental assessment.
+  final double score;
+
+  Report({
+    required this.timestamp,
+    required this.usageMinutes,
+    required this.score
+  });
+
+  /// Constructs a `Report` from a `Map`.
+  Report.fromMap(Map<String, dynamic> map)
+      : timestamp = DateTime.fromMillisecondsSinceEpoch(map['timestamp']),
+        usageMinutes = map['usageMinutes'],
+        score = map['score'];
+
+  /// Converts the `Report` to a `Map` for inserting into a database.
+  Map<String, dynamic> toMap() {
+    return {
+      'timestamp': timestamp.millisecondsSinceEpoch,
+      'usageMinutes': usageMinutes,
+      'score': score
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Report{timestamp: ${timestamp.toString()}, usageMinutes: $usageMinutes, score: $score}';
+  }
+}
